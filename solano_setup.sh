153509c89096ac65a4b8ac3a6ae7514bb3efc76f
Matthew Albani <Matthew.jalbani@gmail.com>
2016-10-20 12:40:08 -0700

    Update solano_setup.sh

--- solano_setup.sh
+++ solano_setup.sh
@@ -10,16 +10,6 @@ sudo apt-get update
 sudo apt-get -y install python-gmpy
 mkdir builds
 pushd builds
-# # Install gmpy2 dependencies
-# mkdir -p $HOME/.local
-# wget ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.2.tar.gz
-# tar xzvf mpc-1.0.2.tar.gz
-# pushd mpc-1.0.2
-# ./configure --prefix=$HOME/.local
-# make
-# make install
-# popd
-# # End install gmpy2 dependencies
 pip install --install-option="--no-cython-compile" Cython==0.22
 pip install $NUMPYSPEC
 pip install nose mpmath argparse Pillow codecov
